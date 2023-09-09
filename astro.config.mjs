a09ca3587acb4360dd0682a4179bfa8d1a410229
Joseph Muller <jtmuller5@gmail.com>
2023-09-09 02:06:13 -0400

    deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,61 +2,75 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from "@astrojs/tailwind";
 
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github.com/CodeOTR',
-    base: '/flutter-fast-guide',
-	integrations: [
-		tailwind(),
-		starlight({
-			
-			favicon: 'favicon.ico',
-			title: 'Flutter Fast',
-			logo: {
-				src: './src/assets/flutter-fast-small.png',
-				
-			},
-			social: {
-				github: 'https://github.com/CodeOTR/flutter-fast-guide',
-				twitter: 'https://twitter.com/CodeOnTheRocks_',
-				threads: 'https://www.threads.net/@codeontherocks',
-			},
-			sidebar: [
-				{
-					label: 'Start Here',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Why Flutter Fast?', link: '/docs/start/why/' },
-						{ label: 'Installation', link: '/docs/start/installation/' },
-						{ label: 'Build an App', link: '/docs/start/build/' },
-						{ label: 'Updates', link: '/docs/start/updates/' },
-					],
-				},
-				{
-					label: 'Setup Steps',
-					items: [
-						{ label: 'Setup?', link: '/docs/setup/overview/' },
-						{ label: 'Firebase', link: '/docs/setup/firebase/' },
-						{ label: 'Supabase', link: '/docs/setup/supabase/' },
-						{ label: 'Sentry', link: '/docs/setup/sentry/' },
-						{ label: 'Amplitude', link: '/docs/setup/amplitude/' },
-						{ label: 'RevenueCat', link: '/docs/setup/revenuecat/' },
-					],
-				},
-				{
-					label: 'Features',
-					items: [
-						{ label: 'List', link: '/docs/features/overview/' },
-					],
-				},
-				{
-					label: 'Roadmap',
-					items: [
-						{ label: 'Planned Features', link: '/docs/roadmap/plan/' },
-						{ label: 'Changelog', link: '/docs/roadmap/changelog/' },
-					],
-				},
-			],
-		}),
-	],
+  site: 'https://github.com/CodeOTR',
+  base: '/flutter-fast-guide',
+  integrations: [tailwind(), starlight({
+    favicon: 'favicon.ico',
+    title: 'Flutter Fast',
+    logo: {
+      src: './src/assets/flutter-fast-small.png'
+    },
+    social: {
+      github: 'https://github.com/CodeOTR/flutter-fast-guide',
+      twitter: 'https://twitter.com/CodeOnTheRocks_',
+      threads: 'https://www.threads.net/@codeontherocks'
+    },
+    sidebar: [{
+      label: 'Start Here',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Why Flutter Fast?',
+        link: '/docs/start/why/'
+      }, {
+        label: 'Installation',
+        link: '/docs/start/installation/'
+      }, {
+        label: 'Build an App',
+        link: '/docs/start/build/'
+      }, {
+        label: 'Updates',
+        link: '/docs/start/updates/'
+      }]
+    }, {
+      label: 'Setup Steps',
+      items: [{
+        label: 'Setup?',
+        link: '/docs/setup/overview/'
+      }, {
+        label: 'Firebase',
+        link: '/docs/setup/firebase/'
+      }, {
+        label: 'Supabase',
+        link: '/docs/setup/supabase/'
+      }, {
+        label: 'Sentry',
+        link: '/docs/setup/sentry/'
+      }, {
+        label: 'Amplitude',
+        link: '/docs/setup/amplitude/'
+      }, {
+        label: 'RevenueCat',
+        link: '/docs/setup/revenuecat/'
+      }]
+    }, {
+      label: 'Features',
+      items: [{
+        label: 'List',
+        link: '/docs/features/overview/'
+      }]
+    }, {
+      label: 'Roadmap',
+      items: [{
+        label: 'Planned Features',
+        link: '/docs/roadmap/plan/'
+      }, {
+        label: 'Changelog',
+        link: '/docs/roadmap/changelog/'
+      }]
+    }]
+  })]
 });
